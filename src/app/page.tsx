3bdb550563ef36b5e1deef5c89340cb876bb4e69
hamid raza <hamid786raz@gmail.com>
2024-12-26 19:00:35 +0500

    Add HeroSection component and spotlight animation; update Tailwind config for color variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/ui/HeroSection";
 
 
 export default function Home() {
   return (
-   <>
-   
-   </>
+   <main className="min-h-screen bg-blacl/[0.96] antialiased bg-grid-white/[0.02]">
+   <HeroSection/>
+   </main>
   );
 }
