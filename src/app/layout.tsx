583824e7634948d85e41fc424244449c77f6028a
hamid raza <hamid786raz@gmail.com>
2024-12-26 09:57:23 +0500

    Add dark mode support and update Navbar component structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/Navbar";
+import Navbar  from "@/components/Navbar";
 
 
 const geistSans = Geist({
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,30 @@
-"use client";
+'use client';
+
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem} from "./ui/navbar-menu";
 import { cn } from "../utils/cn";
+import Link from "next/link";
 
-export function Navbar() {
-  return (
-    <div className="relative w-full flex items-center justify-center">
-      <NavbarDemo className="top-2" />
-      <p className="text-red dark:text-white">
 
-      </p>
-    </div>
-  );
-}
-
-function NavbarDemo({ className }: { className?: string }) {
-  const [active, setActive] = useState<string | null>(null);
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
-      <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
+        <Menu setActive={setActive}>
+            <Link href={"#"}>
+            <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/web-dev">Web Development</HoveredLink>
             <HoveredLink href="/interface-design">Interface Design</HoveredLink>
             <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
             <HoveredLink href="/branding">Branding</HoveredLink>
           </div>
-        </MenuItem>
-        
-        <MenuItem setActive={setActive} active={active} item="Pricing">
+            </MenuItem>
+            </Link>
+
+            <MenuItem setActive={setActive} active={active} item="Pricing">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/hobby">Hobby</HoveredLink>
             <HoveredLink href="/individual">Individual</HoveredLink>
@@ -38,7 +32,18 @@ function NavbarDemo({ className }: { className?: string }) {
             <HoveredLink href="/enterprise">Enterprise</HoveredLink>
           </div>
         </MenuItem>
-      </Menu>
+            <MenuItem setActive={setActive} active={active} item="Hobbies">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/hobby">Hobby</HoveredLink>
+            <HoveredLink href="/individual">Individual</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+          </div>
+        </MenuItem>
+
+        </Menu>
     </div>
-  );
+  )
 }
+
+export default Navbar
